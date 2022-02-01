7e23bbfea489e4fa5689cf96f7155f665ec7420a
miracezra <92326865+miracezra@users.noreply.github.com>
2022-02-01 19:44:27 +0300

    PatikaDev_MainOdev

---
+++ PersonalInfo-HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Your Last Name");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            int age  =int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Your Personal Info :\n Age : {0} -**- First & Last Name : {1} {2}",age,firstName,lastName);
+            Console.ReadLine();
+        }
+    }
+}
